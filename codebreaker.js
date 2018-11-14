7293a1cc886fdcb77f5718f096a4aff6ee33944e
Camila Gomez <camigomez@hugeinc.com>
2018-11-13 21:13:42 -0500

    Update data

--- codebreaker.js
+++ codebreaker.js
@@ -1,9 +1,24 @@
 var realNumber = "1234";
 
- const guess = function(number) {
+const guess = function(number1) {
   // TODO: Logic Here
+  let result = "";
+  console.log(realNumber);
 
-  return "XXXX";
+  let real = realNumber.split("");
+  let actual = number1.split("");
+
+  actual.forEach(element => {
+    // TODO: Si elemnt esta en real entonces agregue una O o una X a result
+  });
+
+
+  return result;
 }
 
-module.exports = guess;
+const setNumber = function (number1) {
+  realNumber = number1;
+}
+
+module.exports.guess = guess;
+module.exports.setNumber = setNumber;
--- test/codebreaker.test.js
+++ test/codebreaker.test.js
@@ -1,15 +1,27 @@
 // Import test
 let test = require('assert');
-var guess = require('../codebreaker');
+var codebreaker = require('../codebreaker');
 
-describe( 'Test pass', function(){
-  it('is true', function(){
-     test.ok(true, true)
+describe('Test pass', function () {
+  it('is true', function () {
+    let saludo = "Hola";
+    test.equal(saludo, "Hola", "Saludo es no igual a Hola");
   })
 })
 
-describe('Codebreaker Test', function(){
-    it('This is a test case', function(){
-        // TODO: Test case here
-    })
+describe('Codebreaker Test', function () {
+
+  beforeEach(function () {
+    codebreaker.setNumber('5612')
+  })
+
+  it('Debe retornar un o', function () {
+    let result = codebreaker.guess("0981");
+    test.equal(result, "o", "Tiene mas o menos de un numero correcto en la posicion equivocada");
+  })
+
+  it('Debe retornar un ox', function () {
+    let result = codebreaker.guess("1928");
+    test.equal(result, "ox", "Esta equivocado");
+  })
 })
